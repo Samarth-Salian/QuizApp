55836043fc04d893c3c7ac52b86e82ce18d45d2c
Samarth-Salian <samarth.salain@i-exceed.com>
2021-02-06 01:34:22 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuizLoginComponent } from './quiz-login/quiz-login.component';
+import { QuizStartComponent } from './quiz-start/quiz-start.component';
+import { QuizDisplayComponent } from './quiz-display/quiz-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizLoginComponent,
+    QuizStartComponent,
+    QuizDisplayComponent
   ],
   imports: [
     BrowserModule,
