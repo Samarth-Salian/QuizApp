68c4c3c21a9c2232416ff8682dbd95b8c51504c2
Pratik Mandal <pratikmandal3001@gmail.com>
2021-02-06 14:02:43 +0530

    Committing header and start page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
 import { RouterModule, Routes } from '@angular/router';
+import { QuizStartComponent } from './quiz-start/quiz-start.component';
+import { QuizHeaderComponent } from './quiz-header/quiz-header.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'start',
+    component: QuizStartComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
