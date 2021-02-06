5bb3df574b8834fc46921e3e89932dc9c2b9d689
bala453a1 <amalakanti.balakrishna@i-exceed.com>
2021-02-06 22:57:47 +0530

    consolidated changes

--- src/app/quiz-container/quiz-container.component.ts
+++ src/app/quiz-container/quiz-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Quiz, Question } from './../quiz-model/Quiz-Model';
+import { Quiz, Question } from './../quiz-model/quiz-model';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-quiz-container',
@@ -8,27 +9,54 @@ import { Quiz, Question } from './../quiz-model/Quiz-Model';
   styleUrls: ['./quiz-container.component.css']
 })
 export class QuizContainerComponent implements OnInit {
-
   quizQuestions: Question[] = [];
-
   currentQuestionIndex = 0;
-
   currentQuestion: Question = {};
-
-  constructor(private http: HttpClient) { }
+  timeLeft: number = 10;
+  progressbarValue:number;
+  interval: any;
+  questionLength :number;
+  questionNumber: number;
+  constructor(private http: HttpClient,private router: Router) {
+    this.questionNumber = 0;
+    this.progressbarValue = 10;
+    this.questionLength = 0;
+   }
 
   ngOnInit(): void {
     this.http.get<Quiz>('assets/mock/questionsNOptions.json').subscribe((data) => {
       this.quizQuestions = data.questions
-      this.nextQuestion();
+      this.questionLength = this.quizQuestions.length;
+      this.nextQuestion(event);
     });
   }
 
-  nextQuestion() {
+  nextQuestion(event:any) {
+    this.questionNumber++;
+    if(this.questionNumber > this.quizQuestions.length){
+      this.router.navigateByUrl('/score');
+    }
     this.currentQuestion = this.quizQuestions[this.currentQuestionIndex];
     this.currentQuestionIndex++;
+    this.timeLeft = 10;
+    if(this.questionNumber === 10){
+      event.target.innerText = "Save";
+    }
   }
-
-
+  startTimer() {
+  this.interval = setInterval(() => {
+      if(this.timeLeft > 0) {
+        this.timeLeft--;
+      }
+    },1000)
+  }
+  ngAfterViewInit(){
+    this.startTimer();
+  }
+  /*nextQuestion () {
+    this.timeLeft = 10;
+    this.questionNumber++;
+    this.progressbarValue = this.progressbarValue + 10;
+  }*/
 
 }
