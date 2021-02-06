efea9c03d1c4836015561cb92f5e5152ec19b0f0
Aishu1410 <aishwarya.munde@i-exceed.com>
2021-02-06 15:09:14 +0530

    Initial Configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { QuizLoginComponent } from './quiz-login/quiz-login.component';
+import { QuizStartComponent } from './quiz-start/quiz-start.component';
+import { QuizScoreComponent } from './quiz-score/quiz-score.component';
+const routes: Routes = [  
+  {
+    path:"",
+    component: QuizLoginComponent
+  },{
+    path:"quiz-start",
+    component: QuizStartComponent
+  },
+  {
+    path:"quiz-score",
+    component: QuizScoreComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
